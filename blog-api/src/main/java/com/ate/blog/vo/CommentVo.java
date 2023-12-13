14abc1b40b0c236d7e723bf37ce0c6b102ae41f5
ye_te <474026268@qq.com>
2023-12-13 16:30:22 +0800

    fix：防止前端 精度损失 把id转为String

--- blog-api/src/main/java/com/ate/blog/vo/CommentVo.java
+++ blog-api/src/main/java/com/ate/blog/vo/CommentVo.java
@@ -1,11 +1,15 @@
 package com.ate.blog.vo;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class CommentVo {
+    // 防止前端 精度损失 把id转为String
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     private UserVo author;
